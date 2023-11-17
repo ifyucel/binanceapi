ebf62bf416db55e6d02290569ac8f458420d45d8
Fatih Yucel <fyc@vava.cars>
2023-11-18 01:09:20 +0300

    unique id implementation

--- src/main/java/com/ticker/binanceapi/model/MarketDataModel.java
+++ src/main/java/com/ticker/binanceapi/model/MarketDataModel.java
@@ -1,14 +1,21 @@
 package com.ticker.binanceapi.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import org.hibernate.annotations.CreationTimestamp;
+import java.util.Date;
 
 @Entity
 public class MarketDataModel {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String symbol;
     private String price;
-
+    @CreationTimestamp
+    private Date timestamp;
     public String getSymbol() {
         return symbol;
     }
